3bd63c13df0eb93d90b98f5052768b7979ddba7f
YAKU-2003 <40yashk@gmail.com>
2026-04-17 03:32:58 -0400

    attempt1

---
+++ attempt1.py
@@ -0,0 +1,18 @@
+import pybullet as p
+import pybullet_data
+import time
+
+p.connect(p.GUI)
+p.setGravity(0, 0, -9.81)
+
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.loadURDF("plane.urdf")
+
+robot = p.loadURDF(
+    "C:/Users/ykulk/Downloads/simulations/attempt1/simready1.urdf",
+    basePosition=[0, 0, 0.2]
+)
+
+while True:
+    p.stepSimulation()
+    time.sleep(1./240.)
